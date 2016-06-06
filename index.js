35a6e8522d9b07298f092caed34534933ed6e7e7
windht <buildmindht@outlook.com>
2016-06-06 18:40:55 +0800

    Connect with ip-api

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+var request = require('request');
+
+var WhoAreYou = function(req,res,next){
+	request('http://ip-api.com/json/'+req.headers['x-forwarded-for'], function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    console.log(body)
+	  }
+	})
+	next();
+}
+
 module.exports = WhoAreYou;
